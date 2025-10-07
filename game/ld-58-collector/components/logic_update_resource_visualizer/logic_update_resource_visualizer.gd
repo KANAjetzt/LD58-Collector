30fe79192632187637deb2cde2826ff590587fc8
KANAjetzt <41547570+KANAjetzt@users.noreply.github.com>
2025-10-07 13:39:34 +0200

    fix: :bug: drone resource storage visualized

--- game/ld-58-collector/components/logic_update_resource_visualizer/logic_update_resource_visualizer.gd
+++ game/ld-58-collector/components/logic_update_resource_visualizer/logic_update_resource_visualizer.gd
@@ -1,11 +1,19 @@
+# TODO: Would be nice to change this so if one resource
+# 		is stored on the drone it allways shows in the center.
 class_name ComponentUpdateResourceVisualizer
 extends ComponentLogicBlock
 
 
-@export var resource_visualizer: ComponentResourceVisualizer
-@export var resource: DataResource
+@export var storage: ComponentStorage
+
+
+func _ready() -> void:
+	get_child(0).texture = storage.resource.sprite
 
 
 func update(_only_return := false) -> Variant:
-	resource_visualizer.update(resource)
+	if storage.current > 0:
+		get_child(0).show()
+	else:
+		get_child(0).hide()
 	return null
--- game/ld-58-collector/units/drone/toggle_sprite.gd
+++ game/ld-58-collector/units/drone/toggle_sprite.gd
@@ -4,19 +4,12 @@ extends ComponentLogicBlock
 @export var storages: Array[ComponentStorage]
 @export var sprites: Array[Sprite2D]
 
+
 func update(_only_return := false) -> Variant:
 	for storage in storages:
-		if storage.resource.id == "resource_battery_empty" and storage.current > 0:
-			sprites[0].show()
-		else:
-			sprites[0].hide()
-		if storage.resource.id == "resource_battery_full" and storage.current > 0:
-			sprites[1].show()
-		else:
-			sprites[1].hide()
-		if storage.resource.id == "resource_ore" and storage.current > 0:
-			sprites[2].show()
-		else:
-			sprites[2].hide()
+		if storage.current > 0:
+			for sprite in sprites:
+				if not sprite.visible:
+					sprite.show()
 
 	return null
